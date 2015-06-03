895e8f8d02b6ab2551a0de0ad183b4091e0f2c14
Albert Albala <albert.albala@linuxfoundation.org>
2015-06-03 11:56:28 -0400

    Issue #8 use different Dockerfile

--- scripts/build-dev.sh
+++ scripts/build-dev.sh
@@ -1,2 +1,8 @@
-docker build -f="Dockerfile-dev" -t docker-dcycleu-dev .
-docker run -d -p 80:80 -v $(pwd):/usr/share/site/ docker-dcycleu-dev
+# Make sure errors propagate throughout the script
+set -e
+
+docker build -f="Dockerfile-jekyll" -t docker-dcycleu-dev-jekyll .
+docker run -d -v $(pwd):/srv/jekyll/ docker-dcycleu-dev-jekyll /bin/bash -c 'jekyll build --watch --force_polling'
+
+docker build -f="Dockerfile-nginx" -t docker-dcycleu-dev-nginx .
+docker run -d -p 80:80 -v $(pwd)/_site:/usr/share/nginx/html/ docker-dcycleu-dev-nginx
