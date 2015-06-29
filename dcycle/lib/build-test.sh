ab904ef20405fd8104d0f5f4930903008e109016
Albert Albala <albert.albala@linuxfoundation.org>
2015-06-29 12:55:17 -0400

    add Dockerfile-test for Jekyll

--- dcycle/lib/build-test.sh
+++ dcycle/lib/build-test.sh
@@ -6,8 +6,8 @@ echo -e "[  >>] Start of script $0"
 PORT=$1
 PROJECTNAME=$2
 
-docker build --no-cache -f="Dockerfile-dcycle-jekyll" -t $PROJECTNAME-jekyll .
-docker run -d -v $(pwd):/srv/jekyll/ $PROJECTNAME-jekyll /bin/bash -c 'jekyll build --watch --force_polling'
+docker build -f="Dockerfile-dcycle-jekyll" -t $PROJECTNAME-jekyll .
+docker run -d $PROJECTNAME-jekyll /bin/bash -c 'jekyll build --watch --force_polling'
 
 docker build -f="Dockerfile-dcycle-nginx" -t $PROJECTNAME-nginx .
 docker run -d -p $PORT:80 -v $(pwd)/_site:/usr/share/nginx/html/ $PROJECTNAME-nginx
