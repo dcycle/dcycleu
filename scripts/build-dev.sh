a06817dc8240a7bcb3c4647129c477b7ff691849
Albert Albala <albert.albala@linuxfoundation.org>
2015-06-29 12:03:49 -0400

    Use latest version of Dcyclebox scripts

--- scripts/build-dev.sh
+++
@@ -1,8 +0,0 @@
-# Make sure errors propagate throughout the script
-set -e
-
-docker build -f="Dockerfile-jekyll" -t docker-dcycleu-dev-jekyll .
-docker run -d -v $(pwd):/srv/jekyll/ docker-dcycleu-dev-jekyll /bin/bash -c 'jekyll build --watch --force_polling'
-
-docker build -f="Dockerfile-nginx" -t docker-dcycleu-dev-nginx .
-docker run -d -p 80:80 -v $(pwd)/_site:/usr/share/nginx/html/ docker-dcycleu-dev-nginx
